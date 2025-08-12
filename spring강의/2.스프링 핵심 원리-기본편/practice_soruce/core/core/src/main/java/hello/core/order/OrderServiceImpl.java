f5e2725c3586679dd63b830a71ef884e8878dedf
ssm <tjtkdan1072@gmail.com>
2025-08-12 21:46:36 +0900

    lombok 추가 및 사용해보기

--- spring강의/2.스프링 핵심 원리-기본편/practice_soruce/core/core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ spring강의/2.스프링 핵심 원리-기본편/practice_soruce/core/core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -4,19 +4,21 @@ import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
 import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.*;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor // final이 붙은 필드의 생성자를 자동으로 만들어준다.
 public class OrderServiceImpl implements OrderService {
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
-    @Autowired
+    /*@Autowired
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
-    }
+    }*/
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
