ba0d8889e830a0e3701d2e8d17d64dcd6aeffc38
ssm <tjtkdan1072@gmail.com>
2025-08-12 19:19:32 +0900

    빈 등록시 충돌 상황 예시

--- spring강의/2.스프링 핵심 원리-기본편/practice_soruce/core/core/src/main/java/hello/core/AutoAppConfig.java
+++ spring강의/2.스프링 핵심 원리-기본편/practice_soruce/core/core/src/main/java/hello/core/AutoAppConfig.java
@@ -1,5 +1,9 @@
 package hello.core;
 
+import hello.core.member.MemberRepository;
+import hello.core.member.MemberService;
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -11,5 +15,10 @@ import static org.springframework.context.annotation.ComponentScan.*;
         excludeFilters = @Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
-
+    // 수동등록 vs 자동등록 빈 충돌발생! 테스트시 수동등록빈이 자동으로 오버라이드 되지만
+    // spring을 돌렸을때는 오버라이드가 기본으로 false 로 설정되어있기 때문에 오류가 발생한다.
+    @Bean(name = "memoryMemberRepository")
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
 }
