04e349a2816171dc964a6a0ba02aaeeb778d0d01
ssm <tjtkdan1072@gmail.com>
2025-08-13 20:32:55 +0900

    ObjectProvider을 사용하여 원하는 빈을 컨테이너에서 찾기

---
+++ spring강의/2.스프링 핵심 원리-기본편/practice_soruce/core/core/src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -0,0 +1,87 @@
+package hello.core.scope;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectFactory;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatException;
+
+public class SingletonWithPrototypeTest1 {
+    @Test
+    void prototypeFind() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(PrototypeBean.class);
+        PrototypeBean protoypeBean1 = ac.getBean(PrototypeBean.class);
+        protoypeBean1.addCount();
+        assertThat(protoypeBean1.getCount()).isEqualTo(1);
+
+        PrototypeBean protoypeBean2 = ac.getBean(PrototypeBean.class);
+        protoypeBean2.addCount();
+        assertThat(protoypeBean2.getCount()).isEqualTo(2);
+
+    }
+
+    @Test
+    void singletonClientUsePrototype() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext
+                (ClientBean.class, PrototypeBean.class);
+
+        ClientBean clientBean1 = ac.getBean(ClientBean.class);
+        int count1 = clientBean1.logic();
+        assertThat(count1).isEqualTo(1);
+
+        ClientBean clientBean2 = ac.getBean(ClientBean.class);
+        int count2 = clientBean1.logic();
+        assertThat(count2).isEqualTo(1);
+
+    }
+
+    @Scope("singleton")
+    static class ClientBean {
+        // private final PrototypeBean prototypeBean; // 생서시점에 주입, 문제-> 프로토타입 형식의 빈을 불러올수없다.
+
+        /*public ClientBean(PrototypeBean prototypeBean) {
+            this.prototypeBean = prototypeBean;
+        }*/
+
+        @Autowired
+        ObjectProvider<PrototypeBean> prototypeBeanProvider; // ObjectProvider사용, ObjectFactory도 가능(ObjectFactory을 상속받은것)
+
+        public int logic() {
+            PrototypeBean prototypeBean = prototypeBeanProvider.getObject();
+            prototypeBean.addCount();
+            int count = prototypeBean.getCount();
+            return count;
+        }
+
+    }
+
+    @Scope("prototype")
+    static class PrototypeBean {
+        private int count = 0;
+
+        public void addCount() {
+            count++;
+        }
+
+        public int getCount() {
+            return count;
+        }
+
+        @PostConstruct
+        public void init() {
+            System.out.println("PrototypeBean.init" + this);
+        }
+
+        @PreDestroy
+        public void destroy() {
+            System.out.println("PrototypeBean.destroy");
+        }
+    }
+}
